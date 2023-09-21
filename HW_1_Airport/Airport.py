7f0bbcdf828a5d48d791f7e814334032280df70e
Mya Schroder <80082701+schromya@users.noreply.github.com>
2023-09-21 11:15:58 -0800

    Psuedo Code functions

---
+++ HW_1_Airport/Airport.py
@@ -0,0 +1,19 @@
+
+class Airport:
+
+
+    def __init__(self, name:str, IATA_port_mapping:dict):
+        self.name = name
+        self.IATA_port_mapping = IATA_port_mapping
+
+    def start_server(self):
+        pass
+
+    def start_client(self):
+        pass
+
+    def receive_passenger(self):
+        pass
+
+    def send_passenger(self):
+        pass
