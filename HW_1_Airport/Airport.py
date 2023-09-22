c06605bb345154b557083da29febbd01bfe04918
Mya Schroder <80082701+schromya@users.noreply.github.com>
2023-09-22 09:35:29 -0800

    More setup

--- HW_1_Airport/Airport.py
+++ HW_1_Airport/Airport.py
@@ -2,18 +2,26 @@
 class Airport:
 
 
-    def __init__(self, name:str, IATA_port_mapping:dict):
+    def __init__(self, name:str, IATA_port_mapping:dict, connected_airports: list) -> None:
         self.name = name
         self.IATA_port_mapping = IATA_port_mapping
+        self.connected_airports = connected_airports
 
-    def start_server(self):
+    def start_server(self) -> None:
+        """
+        Starts TCP server for this airport.
+        """
+        
         pass
 
-    def start_client(self):
+    def start_client(self) -> None:
+        """
+        Starts TCP client for this airport.
+        """
         pass
 
-    def receive_passenger(self):
+    def receive_passenger(self) -> None:
         pass
 
-    def send_passenger(self):
+    def send_passenger(self) -> None:
         pass
---
+++ HW_1_Airport/main.py
@@ -0,0 +1,17 @@
+
+
+
+def main():
+    IATA_port_mapping = {
+        "ANC":8080,
+        "SEA":8081,
+        "FAE":8082,
+        "BRW":8083,
+        "OTZ":8084
+    }
+    
+    pass
+
+
+if __name__ == "main":
+    main()
